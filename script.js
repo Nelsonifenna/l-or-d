8b6f3fdb6e240d70ed5b3c5a6b9aeb075a5fd981
NelsonJosep8 <nelsonjosep240@gmail.com>
2025-07-28 00:00:33 +0100

    l

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// nelson
+const deathBtn = document.getElementById("deathBtn");
+const bornBtn = document.getElementById("bornBtn");
+const liveBtn = document.getElementById("liveBtn");
+const countlabel = document.getElementById("countlabel");
+let count = 0;
+
+liveBtn.onclick = function () {
+  count++;
+  countlabel.textContent = count;
+};
+deathBtn.onclick = function () {
+  count--;
+  countlabel.textContent = count;
+};
+bornBtn.onclick = function () {
+  count = 0;
+  countlabel.textContent = count;
+};
